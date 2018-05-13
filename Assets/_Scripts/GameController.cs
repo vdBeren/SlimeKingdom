2ae9c7dadc42db08d0d4e321e6e3d224ad8c4d7f
vdBeren <vdias.savariego@gmail.com>
2018-05-13 20:32:35 -0300

    Made a little paint prototype

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -1,73 +1,184 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using DG.Tweening;
 
-public class GameController : MonoBehaviour {
+namespace SlimeKingdom{
+	public class GameController : MonoBehaviour {
 
 
-	public GameObject tileObject;
-	public GameObject gridParent;
+		public GameObject pixelObject;
+		public GameObject gridParent;
 
-	public int cellSize = 1;
-	public int gridSize = 1;
+		public int cellSize = 1;
+		public int gridSize = 1;
 
-	private GameObject[,] grid;
+		public Text progressText;
+		private float progressValue = 0.0f;
 
+		private GameObject[,] grid;
 
-	//TODO: Keep track of player movement on the Grid. Enable / Disable Tile colliders based on this, to allow selection. Make UI for this.
-	//TODO: Tiles own class for better code.
-	//TODO: Turn logic, alternating between N players. Make UI for this.
-	//TODO: King logic, placing different types of units while moving
+		private GameObject[] allPixels;
+		private Vector2 gridParentPosition;
 
-	public void CreateBattlefieldGrid(){
+		//TODO: Keep track of player movement on the Grid. Enable / Disable Tile colliders based on this, to allow selection. Make UI for this.
+		//TODO: Tiles own class for better code.
+		//TODO: Turn logic, alternating between N players. Make UI for this.
+		//TODO: King logic, placing different types of units while moving
 
-		grid = new GameObject[gridSize, gridSize];
-
-		var tParent = gridParent.transform;
-		tParent.DOMove (new Vector2(tParent.position.x + (cellSize * -gridSize)/2.0f, tParent.position.y + (cellSize * gridSize)/2.0f), 0.9f);
-		
-		for (int i = 0; i < gridSize; i++) {
-			for (int j = 0; j < gridSize; j++) {
-
-				var newTile = Instantiate (tileObject);
-				newTile.transform.parent = gridParent.transform;
-
-				grid [i, j] = newTile;
-
-				var t = newTile.transform;
-				t.DOLocalMove (new Vector2 ((t.position.x + cellSize) * j, (t.position.y + cellSize) * -i), 0.4f).SetDelay (0.1f * j);
+		public Texture2D tex;
 
 
-			}
-		}
+		public void CreateImageByPixels(){
 
-	}
+			ClearImage ();
 
-	public void CheckForPlayerInput(){
-		
-		if (Input.GetMouseButtonDown (0)) {
-			Ray r = Camera.main.ScreenPointToRay (Input.mousePosition);
-			RaycastHit2D hit = Physics2D.Raycast(r.origin, r.direction, Mathf.Infinity);
+			float pixelSize = pixelObject.transform.localScale.x;
 
-			if (hit){
-				if (hit.collider.gameObject.tag == "Tile") {
-					SpriteRenderer sr = hit.collider.gameObject.GetComponent<SpriteRenderer> ();
-					sr.DOColor (new Color (1, 0, 0), 0.4f);
+			Color[] imagePixels = tex.GetPixels ();
+			System.Array.Reverse (imagePixels);
+
+			gridParent.transform.DOMove (new Vector2(gridParentPosition.x + (pixelSize * tex.height)/2.0f, gridParentPosition.y + (pixelSize * tex.width)/2.0f), 0.9f);
+
+			for (int i = 0; i < tex.height; i++) {
+				for (int j = 0; j < tex.width; j++) {
+
+					int pixelIndex = (i * tex.width) + j;
+
+					//Debug.Log (pixels[pixelIndex]);
+					//Debug.Log ("i = " + i);
+					//Debug.Log ("j = " + j);
+
+					if (imagePixels [pixelIndex].a <= 0.0f)
+						continue;
+
+					StartCoroutine(SpawnPixels(i, j, pixelIndex, imagePixels, pixelSize));
+
 				}
 			}
+
+
+		}
+
+		IEnumerator SpawnPixels(int i, int j, int pixelIndex, Color[] imagePixels, float pixelSize){
+			
+			yield return new WaitForSeconds (Random.Range(0.1f, 0.4f));
+
+			var newPixel = Instantiate (pixelObject, gridParent.transform);
+			var pixel = newPixel.GetComponent<Pixel> ();
+
+			var color = new Color (imagePixels [pixelIndex].r, imagePixels [pixelIndex].g, imagePixels [pixelIndex].b);
+			pixel.SetColor (color);
+
+			newPixel.transform.DOLocalMove (new Vector2 ((pixelSize) * -j, (pixelSize) * -i), 0.4f).SetDelay(0.02f * j);
+
+		}
+
+		public void ToggleImageColliders(){
+
+			GetAllChildren ();
+
+			foreach (GameObject child in allPixels) {
+				
+				var pixel = child.GetComponent<Pixel> ();
+				pixel.ToggleCollider ();
+
+			}
+		}
+
+		private void ClearImage(){
+
+			GetAllChildren ();
+
+			foreach (GameObject child in allPixels) {
+				Destroy (child.gameObject);
+			}
+		}
+			
+		private void GetAllChildren(){
+			
+			allPixels = new GameObject[gridParent.transform.childCount];
+			int count = 0;
+
+			foreach (Transform child in gridParent.transform) {
+				allPixels [count] = child.gameObject;
+				count++;
+			}
+		}
+
+		private void GetProgress(){
+
+			int paintedPixels = 0;
+
+			foreach (GameObject pixel in allPixels) {
+				if (pixel.GetComponent<Pixel> ().isPainted)
+					paintedPixels++;
+			}
+				
+			progressValue = (float) (paintedPixels / (float) allPixels.Length) * 100;
+			progressText.text = progressValue.ToString("F1") + "%";
+		}
+
+		private void CheckForPlayerInput(){
+			
+			if (Input.GetMouseButton (0) || Input.GetMouseButtonDown (0)) {
+				Ray r = Camera.main.ScreenPointToRay (Input.mousePosition);
+				RaycastHit2D[] hits = Physics2D.CircleCastAll(r.origin, 0.4f, r.direction);
+
+
+				foreach(RaycastHit2D hit in hits){
+					if (hit){
+						if (hit.collider.gameObject.tag == "Tile") {
+							var pixel = hit.collider.gameObject.GetComponent<Pixel> ();
+							pixel.PaintColor ();
+							GetProgress ();
+						}
+					}
+				}
+
+			}
+
+		}
+
+		// Use this for initialization
+		void Start () {
+			
+			gridParentPosition = gridParent.transform.position;
+			CreateImageByPixels ();
+
+			progressText.text = "0%";
+			//CreateBattlefieldGrid ();
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			CheckForPlayerInput ();
+		}
+
+		public void CreateBattlefieldGrid(){
+
+			grid = new GameObject[gridSize, gridSize];
+
+			var tParent = gridParent.transform;
+			tParent.DOMove (new Vector2(tParent.position.x + (cellSize * -gridSize)/2.0f, tParent.position.y + (cellSize * gridSize)/2.0f), 0.9f);
+
+			for (int i = 0; i < gridSize; i++) {
+				for (int j = 0; j < gridSize; j++) {
+
+					var newTile = Instantiate (pixelObject);
+					newTile.transform.parent = gridParent.transform;
+
+					grid [i, j] = newTile;
+
+					var t = newTile.transform;
+					t.DOLocalMove (new Vector2 ((t.position.x + cellSize) * j, (t.position.y + cellSize) * -i), 0.4f).SetDelay (0.1f * j);
+
+
+				}
+			}
+
 		}
 
 	}
-
-	// Use this for initialization
-	void Start () {
-		CreateBattlefieldGrid ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		CheckForPlayerInput ();
-	}
-
 }
---
+++ Assets/_Scripts/Pixel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pixel : MonoBehaviour {
+
+	public Color originalColor { get; set; }
+	public Color grayColor { get; set; }
+
+	public bool isPainted { get; set;}
+
+	private BoxCollider2D pixelCollider;
+	private SpriteRenderer spriteRenderer;
+
+
+	void Awake(){
+
+		isPainted = false;
+
+		pixelCollider = this.GetComponent<BoxCollider2D> ();
+		spriteRenderer = this.GetComponent<SpriteRenderer> ();
+
+	}
+
+
+
+	public void SetColor(Color newColor){
+		originalColor = newColor;
+		grayColor = new Color(newColor.grayscale, newColor.grayscale, newColor.grayscale);
+
+		spriteRenderer.color = grayColor;
+	}
+
+	public void PaintColor (){
+		
+		if (!isPainted)
+			spriteRenderer.color = originalColor;
+
+		isPainted = true;
+;
+	}
+
+	public void ToggleCollider(){
+		pixelCollider.enabled = !pixelCollider.enabled;
+	}
+}
